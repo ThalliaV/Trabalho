2f959cd01091c11b10b26c5c6d38b20bef72372a
ThalliaV <74866332+ThalliaV@users.noreply.github.com>
2025-09-13 19:04:47 -0300

    App.js

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#be71adff',
+    backgroundColor: '#64369dff',
     alignItems: 'center',
     justifyContent: 'center',
   },
